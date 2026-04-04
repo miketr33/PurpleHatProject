af9cfdf1182305ad0c1290e82137d5acaa09c838
Mike Tree <mikejtree@gmail.com>
2026-04-04 11:50:36 +0100

    feat: Login/Logout

---
+++ PurpleHatProject/Migrations/20260404104501_AddUsers.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PurpleHatProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Mike" },
+                    { 2, "Jordan" },
+                    { 3, "Jason" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ PurpleHatProject/Models/User.cs
@@ -0,0 +1,7 @@
+namespace PurpleHatProject.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
